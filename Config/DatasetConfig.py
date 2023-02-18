06ff305a98f9aba1b562d06bab23bd1b92261251
mahdi <mahdid.m.2000@gmail.com>
2023-02-19 00:22:32 +0330

    train set 2 -> ?

--- Config/DatasetConfig.py
+++ Config/DatasetConfig.py
@@ -1,5 +1,5 @@
 
-hight = 480
-width = 640
+hight = 400
+width = 400
 train_data_amount = 800
 test_data_amount = 200
--- models/Unet.py
+++ models/Unet.py
@@ -5,23 +5,29 @@ class UNET(nn.Module):
 
     def __init__(self):
         super(UNET, self).__init__()
-        self.down1 = self.down_conv_block(n=1, nIn=3, nOut=8)
-        self.down2 = self.down_conv_block(n=2, nIn=8, nOut=32)
-        self.up1 = self.up_conv_block(n=4,nIn=32,nOut=8)
-        self.up2 = self.up_conv_block(n=5,nIn=8,nOut=1)
+        self.down1 = self.down_conv_block(n=1, nIn=3, nOut=8, is_pooling=True)
+        self.down2 = self.down_conv_block(n=2, nIn=8, nOut=32, is_pooling=False)
+        self.down3 = self.down_conv_block(n=3, nIn=32, nOut=128, is_pooling=True)
+        self.down4 = self.down_conv_block(n=4, nIn=128, nOut=512, is_pooling=False)
+        self.up1 = self.up_conv_block(n=4,nIn=512,nOut=128, is_upsample=False)
+        self.up2 = self.up_conv_block(n=5,nIn=128,nOut=32, is_upsample=True)
+        self.up3 = self.up_conv_block(n=6,nIn=32,nOut=8, is_upsample=False)
+        self.up4 = self.up_conv_block(n=7,nIn=8,nOut=1, is_upsample=True)
 
-    def down_conv_block(self,n,nIn,nOut,batch_norm=True ):
+    def down_conv_block(self,n,nIn,nOut,is_pooling,batch_norm=True ):
         block = nn.Sequential()
         block.add_module(f"down_conv_{n}",nn.Conv2d(nIn,nOut,kernel_size=5, stride=1, padding=0))
         if(batch_norm):
             block.add_module(f"down_bn_{n}", nn.BatchNorm2d(nOut))
         block.add_module(f"down_act_{n}", nn.ReLU())
-        block.add_module(f"downsample_{n}", nn.MaxPool2d(kernel_size=2))
+        if(is_pooling):
+            block.add_module(f"downsample_{n}", nn.MaxPool2d(kernel_size=2))
         return block
 
-    def up_conv_block(self,n,nIn,nOut,batch_norm=True):
+    def up_conv_block(self,n,nIn,nOut,is_upsample,batch_norm=True):
         block = nn.Sequential()
-        block.add_module(f"upsampling_{n}", nn.Upsample(scale_factor=2,mode='bilinear'))
+        if(is_upsample):
+            block.add_module(f"upsampling_{n}", nn.Upsample(scale_factor=2,mode='bilinear'))
         block.add_module(f"up_conv",nn.ConvTranspose2d(nIn,nOut,kernel_size=5,stride=1,padding=0))
         if(batch_norm):
             block.add_module(f"up_bn_{n}", nn.BatchNorm2d(nOut))
@@ -32,8 +38,12 @@ class UNET(nn.Module):
     def forward(self, X):
         X = self.down1(X)
         X = self.down2(X)
+        X = self.down3(X)
+        X = self.down4(X)
         X = self.up1(X)
         X = self.up2(X)
+        X = self.up3(X)
+        X = self.up4(X)
         return X
 
 
